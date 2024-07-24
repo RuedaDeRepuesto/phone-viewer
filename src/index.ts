65eafacce2185a0c33f66f6b5507ea28d10ba665
Jorge Videla <jorge.videla.v@gmail.com>
2024-07-24 17:41:11 -0400

    fix emulation

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,30 @@ const createWindow = (): void => {
       webSecurity:false,
       allowRunningInsecureContent:true,
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+      additionalArguments: ['--touch-events']
     },
   });
 
+  try {
+    // works with 1.1 too
+    mainWindow.webContents.debugger.attach('1.2')
+  } catch (err) {
+    console.log('Debugger attach failed: ', err)
+  }
+
+  const isDebuggerAttached = mainWindow.webContents.debugger.isAttached()
+  console.log('debugger attached? ', isDebuggerAttached)
+
+  mainWindow.webContents.debugger.on('detach', (event, reason) => {
+    console.log('Debugger detached due to: ', reason)
+  });
+  mainWindow.webContents.debugger.sendCommand('Emulation.setEmitTouchEventsForMouse', { enabled: true });
+  // This is where the magic happens!
+  mainWindow.webContents.debugger.sendCommand('Emulation.setTouchEmulationEnabled', {
+    enabled: true,
+    configuration: 'mobile',
+  });
+  
   ipcMain.on('app-close', (event) => {
     app.exit();
   });
